92a1b4b55a3098529b477f4f08514cc488e2df46
Tathagata Mondal <tathagata.mandal@zimyo.com>
2023-11-28 20:59:16 +0530

    added phone no and names textbox and morrefinement

--- app/registration/components/registrationForm.tsx
+++ app/registration/components/registrationForm.tsx
@@ -10,49 +10,94 @@ function RegistrationForm() {
     register,
     handleSubmit,
     formState: { errors },
+    watch,
   } = useForm({
     resolver: yupResolver(registrationSchema),
   });
+
+  const checked = watch("checked");
   const onSubmit = (data: any) => console.log(data);
+
   return (
     <div>
       <section className="bg-gray-50 dark:bg-gray-900">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <a
-            href="#"
-            className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-          >
-            <img
-              className="w-8 h-8 mr-2"
-              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-              alt="logo"
-            />
-            Flowbite
-          </a>
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Create an account
-              </h1>
+              <div className="flex gap-3 justify-between">
+                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Create an account
+                </h1>
+                <img
+                  className="w-8 h-8 mr-2"
+                  src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+                  alt="logo"
+                />
+              </div>
+
               <form
                 className="space-y-4 md:space-y-6"
                 action="#"
                 onSubmit={handleSubmit(onSubmit)}
               >
-                <div>
-                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                    Your email
-                  </label>
-                  <input
-                    id="email"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="name@company.com"
-                    {...register("email")}
-                  />
-                  {errors && errors.email && (
-                    <Errormessage message={errors.email.message} />
-                  )}
+                <div className="flex gap-3">
+                  <div>
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      Firstname
+                    </label>
+                    <input
+                      id="firstName"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      {...register("firstName")}
+                    />
+                    {errors && errors.firstName && (
+                      <Errormessage message={errors.firstName.message} />
+                    )}
+                  </div>
+                  <div>
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      Lastname
+                    </label>
+                    <input
+                      id="lastName"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      {...register("lastName")}
+                    />
+                    {errors && errors.lastName && (
+                      <Errormessage message={errors.lastName.message} />
+                    )}
+                  </div>
                 </div>
+                <div className="flex gap-3">
+                  <div>
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      Your email
+                    </label>
+                    <input
+                      id="email"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="name@company.com"
+                      {...register("email")}
+                    />
+                    {errors && errors.email && (
+                      <Errormessage message={errors.email.message} />
+                    )}
+                  </div>
+                  <div>
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      Phone No
+                    </label>
+                    <input
+                      id="phoneNo"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      {...register("phoneNo")}
+                    />
+                    {errors && errors.phoneNo && (
+                      <Errormessage message={errors.phoneNo.message} />
+                    )}
+                  </div>
+                </div>
+
                 <div>
                   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                     Password
@@ -106,7 +151,8 @@ function RegistrationForm() {
                 </div>
                 <button
                   type="submit"
-                  className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                  disabled={!checked}
+                  className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 disabled:opacity-50 disabled:cursor-not-allowed"
                 >
                   Create an account
                 </button>
--- app/registration/domain.ts
+++ app/registration/domain.ts
@@ -2,6 +2,15 @@ import * as yup from "yup";
 
 export const registrationSchema = yup
   .object({
+    firstName: yup.string().required("Firstname is required"),
+    lastName: yup.string().required("Lastname is required"),
+    phoneNo: yup
+      .string()
+      .matches(/^[6-9]\d{9}$/, {
+        message: "Please enter valid number.",
+        excludeEmptyString: false,
+      })
+      .required("Phone No is required"),
     email: yup
       .string()
       .email("Please enter a valie email Id")
