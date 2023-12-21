cb420a4ab47ab1d0c6385c00d8a54d35a4ec13a2
Tathagata Mondal <tathagata.mandal@zimyo.com>
2023-12-22 01:28:18 +0530

    ..bug fix ins registration api

--- app/registration/service.ts
+++ app/registration/service.ts
@@ -7,7 +7,10 @@ async function registerUser(data: registrationObj) {
   if (response.data && response.data.success) {
     return { success: true, message: "Registration is successfull" };
   }
-  return { success: false, message: "Registration is failed" };
+  return {
+    success: false,
+    message: response.data ? response.data.message : "Failed to register",
+  };
 }
 
 export { registerUser };
