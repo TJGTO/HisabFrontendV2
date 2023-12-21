6638533aab298afec8c73aba6a2b114ab2199d4b
Tathagata Mondal <tathagata.mandal@zimyo.com>
2023-12-21 16:41:55 +0530

    ...added links in footer

--- app/Footer/footer.tsx
+++ app/Footer/footer.tsx
@@ -20,13 +20,21 @@ function Footer() {
               </h2>
               <ul className="text-gray-500 dark:text-gray-400 font-medium">
                 <li className="mb-4">
-                  <a href="#" className="hover:underline">
+                  <a
+                    target="_blank"
+                    href="mailto: weekendfootballtournament23@gmail.com"
+                    className="hover:underline"
+                  >
                     Email
                   </a>
                 </li>
                 <li>
-                  <a href="#" className="hover:underline">
-                    Phone
+                  <a
+                    target="_blank"
+                    href="https://chat.whatsapp.com/Du558LDd6Na9f0hWYbtUxk"
+                    className="hover:underline"
+                  >
+                    Whatsapp
                   </a>
                 </li>
               </ul>
@@ -38,7 +46,8 @@ function Footer() {
               <ul className="text-gray-500 dark:text-gray-400 font-medium">
                 <li className="mb-4">
                   <a
-                    href="https://github.com/themesberg/flowbite"
+                    target="_blank"
+                    href="https://www.youtube.com/@WeekendFootballKolkata"
                     className="hover:underline "
                   >
                     Youtube
@@ -46,7 +55,8 @@ function Footer() {
                 </li>
                 <li className="mb-4">
                   <a
-                    href="https://discord.gg/4eeurUVvTy"
+                    target="_blank"
+                    href="https://www.facebook.com/groups/1545945839228827"
                     className="hover:underline"
                   >
                     Facebook
@@ -54,7 +64,8 @@ function Footer() {
                 </li>
                 <li>
                   <a
-                    href="https://discord.gg/4eeurUVvTy"
+                    target="_blank"
+                    href="https://www.instagram.com/wfg_kol/"
                     className="hover:underline"
                   >
                     Instagram
@@ -82,17 +93,21 @@ function Footer() {
           </div>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <div className="sm:flex sm:items-center sm:justify-between">
+        <div className="items-center text-center">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © 2023{" "}
-            <a href="https://flowbite.com/" className="hover:underline">
-              Weekend Football Tournament
+            <a
+              href="https://www.facebook.com/groups/1545945839228827"
+              className="hover:underline"
+            >
+              Weekend Football Group
             </a>
             . All Rights Reserved.
           </span>
-          <div className="flex mt-4 sm:justify-center sm:mt-0">
+          {/* <div className="flex mt-4 sm:justify-center sm:mt-0">
             <a
-              href="#"
+              target="_blank"
+              href="https://www.facebook.com/groups/1545945839228827"
               className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
             >
               <svg
@@ -164,25 +179,25 @@ function Footer() {
               <span className="sr-only">GitHub account</span>
             </a>
             <a
-              href="#"
-              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+              target="_blank"
+              href="https://wa.me/1234567890" // Replace with your WhatsApp number
+              className="text-green-500 hover:text-green-700 dark:hover:text-white flex items-center"
             >
               <svg
-                className="w-4 h-4"
-                aria-hidden="true"
+                className="w-4 h-4 mr-1"
                 xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 20"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               >
-                <path
-                  fill-rule="evenodd"
-                  d="M10 0a10 10 0 1 0 10 10A10.009 10.009 0 0 0 10 0Zm6.613 4.614a8.523 8.523 0 0 1 1.93 5.32 20.094 20.094 0 0 0-5.949-.274c-.059-.149-.122-.292-.184-.441a23.879 23.879 0 0 0-.566-1.239 11.41 11.41 0 0 0 4.769-3.366ZM8 1.707a8.821 8.821 0 0 1 2-.238 8.5 8.5 0 0 1 5.664 2.152 9.608 9.608 0 0 1-4.476 3.087A45.758 45.758 0 0 0 8 1.707ZM1.642 8.262a8.57 8.57 0 0 1 4.73-5.981A53.998 53.998 0 0 1 9.54 7.222a32.078 32.078 0 0 1-7.9 1.04h.002Zm2.01 7.46a8.51 8.51 0 0 1-2.2-5.707v-.262a31.64 31.64 0 0 0 8.777-1.219c.243.477.477.964.692 1.449-.114.032-.227.067-.336.1a13.569 13.569 0 0 0-6.942 5.636l.009.003ZM10 18.556a8.508 8.508 0 0 1-5.243-1.8 11.717 11.717 0 0 1 6.7-5.332.509.509 0 0 1 .055-.02 35.65 35.65 0 0 1 1.819 6.476 8.476 8.476 0 0 1-3.331.676Zm4.772-1.462A37.232 37.232 0 0 0 13.113 11a12.513 12.513 0 0 1 5.321.364 8.56 8.56 0 0 1-3.66 5.73h-.002Z"
-                  clip-rule="evenodd"
-                />
+                <path d="M21 2a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2zm-6.1 13.8c-1.3 1.2-3.1 1.9-5 1.9-4 0-7.3-3.3-7.3-7.4s3.3-7.4 7.3-7.4 7.3 3.3 7.3 7.4c0 2.5-1.2 4.9-3.2 6.5l-.2.2z" />
               </svg>
-              <span className="sr-only">Dribbble account</span>
+              <span className="sr-only">WhatsApp</span>
             </a>
-          </div>
+          </div> */}
         </div>
       </div>
     </footer>
