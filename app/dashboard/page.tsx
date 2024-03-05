92273af06deade2876dfa5b777618a4d93f6e3ff
Tathagata Mondal <tathagata.mandal@zimyo.com>
2024-03-05 18:55:55 +0530

    banner added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import Slider from "./components/slider";
+import Banner from "../Common/banner";
 import StatisticsSection from "../Common/SectionComponents/statisticsSection";
 import CardSection from "./components/cardSection";
 
 export default function Page() {
   return (
     <>
+      <Banner />
       <Slider />
       <StatisticsSection />
       <CardSection />
