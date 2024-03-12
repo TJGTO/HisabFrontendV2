4782e35f00a27734b6d528a78a4049aa0e1c9b3c
Tathagata Mondal <tathagata.mandal@zimyo.com>
2024-03-12 14:11:02 +0530

    ..fix of players type dropdown

--- app/gamedetails/components/tournamentFrom.tsx
+++ app/gamedetails/components/tournamentFrom.tsx
@@ -83,8 +83,8 @@ function TournamentForm({
             className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           >
             <option value="">{"Please Select"}</option>
-            <option value="Player">{"Regular Player"}</option>
-            <option value="Player">{"Non-Regular Player"}</option>
+            <option value="Regular Player">{"Regular Player"}</option>
+            <option value="Non-Regular Player">{"Non-Regular Player"}</option>
             <option value="Regular Owner">{"Regular Owner"}</option>
             <option value="Non-Regular Owner">{"Non-Regular Owner"}</option>
           </select>
